891c3f2d7f68e2e3b470134d56009fa678a16dc3
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-06-26 17:31:07 +0200

    Update Plane.cs
    comments

--- Assets/Input/Plane.cs
+++ Assets/Input/Plane.cs
@@ -8,18 +8,20 @@ public class Plane : MonoBehaviour
     public GameObject enemyPrefab;
     public GameObject spawnPoint;
     public GameObject weapon;
+    
     void Start()
     {
         weapon = GameObject.FindGameObjectWithTag("Weapon");
     }
 
+    //When the collision between weapon and cube, placed inside the gate, 
+    //occurs, the final giant is instantiated, if it has not already been done
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Weapon"))
         {
             if (bigenemyAlive < 1)
             {
-                //Debug.Log("Trigger hit!");
                 Instantiate(enemyPrefab, spawnPoint.transform.position, Quaternion.identity);
                 bigenemyAlive++;
                 Destroy(gameObject);
