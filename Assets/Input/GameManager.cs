6cbcafcd2c666129cc53d58a5d9231292dda4358
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-06-26 17:29:57 +0200

    Update GameManager.cs
    comments

--- Assets/Input/GameManager.cs
+++ Assets/Input/GameManager.cs
@@ -17,19 +17,19 @@ public class GameManager : MonoBehaviour
     public GameObject doorL;
     public GameObject doorR;
     
+    //The round is increased each time an enemy is killed.
+    //Then the NextWave function is recalled.
     void Update() {
         if(enemyAlive == 0){
             round++;
             NextWave(round);
             roundNumber.text ="ROUND: " + round.ToString();
-            /*if (round == 2)
-            { Debug.Log( ("Open Doors"));
-              doorL.transform.Rotate(0f,-90f, 0f, Space.World); 
-              doorL.transform.Rotate(0f,-90f, 0f, Space.World);  
-            }*/
         }
     }
 
+    //NextWave method used to generate giant. Each time there is one killed, 
+    //as many are generated equal to the number of the round.
+    //Enemies are instantiated at randomly chosen spawnPoints.
    public void NextWave (int round) {
          for(var x = 0; x < round; x++){
             GameObject spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
@@ -38,23 +38,32 @@ public class GameManager : MonoBehaviour
             enemySpawned.GetComponent<EnemyManager>().gameManager = GetComponent<GameManager>();
             enemyAlive++;
         }
-    }   
+    } 
+    
+   //Methods used to upload the victory scene
    public void WinGame() {
        Time.timeScale = 0;
        Cursor.lockState = CursorLockMode.None;
        SceneManager.LoadScene(2);
    }
    
+   //Methods used to implement the buttons of the various screens
+   
+   //Restart button: start a new game
     public void Restart () {
         Debug.Log("Restart");
         Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
+    
+    //Back to main menu button: reload the start menu scene
     public void BackToMainMenu() {
         Debug.Log("Back to Main Menu");
         Time.timeScale = 1;
         SceneManager.LoadScene(0);
     }
+    
+     //Methods used upload the loser scene 
     public void EndGame() {
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
