2ae33d8244c8e5ccd6ee485b8403f0554451b32b
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-06-26 17:21:34 +0200

    Update WinnerMenu.cs
    Adding Comments

--- Assets/Scripts/WinnerMenu.cs
+++ Assets/Scripts/WinnerMenu.cs
@@ -3,14 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//Implementation of the scene containing the winner menu 
 public class WinnerMenu : MonoBehaviour
 {
+    //Restart button: starting new game
     public void Restart () {
         Debug.Log("Restart");
         Time.timeScale = 1;
         SceneManager.LoadScene(1);
     }
 
+    //Back to main menu button: used to load the
+    //initial scene containing the main menu 
     public void BackToMainMenu() {
         Debug.Log("Back to Main Menu");
         Time.timeScale = 1;
