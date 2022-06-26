0cf5f8e98cf7a49c76fb3d987936e880e7e56fa2
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-06-26 17:35:19 +0200

    Update RotateObject.cs
    comments

--- Assets/Input/RotateObject.cs
+++ Assets/Input/RotateObject.cs
@@ -6,9 +6,7 @@ public class RotateObject : MonoBehaviour
 {
     [SerializeField] private float _rotateX = 0f, _rotateY = 15f, _rotateZ = 0f;
 
-
-
-    // Update is called once per frame
+   //Used to rotate the weapon in the scene before it is picked up by the player
     void Update()
     {
         transform.Rotate(new Vector3(_rotateX,_rotateY,_rotateZ)* Time.deltaTime);
