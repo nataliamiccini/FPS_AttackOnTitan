4e8e8d7bc012fdeec7c2373285d56582cadf381f
Natalia Miccini <55144535+nataliamiccini@users.noreply.github.com>
2022-06-26 17:31:45 +0200

    Update PlayerManager.cs
    comments

--- Assets/importare/PlayerManager.cs
+++ Assets/importare/PlayerManager.cs
@@ -9,6 +9,10 @@ public class PlayerManager : MonoBehaviour
     public float health = 100;
     public Text healthText;
     public GameManager gameManager;
+    
+    //Hit method used to manage player's life.
+    //If it drops below zero, the game has been lost 
+    //and the End Game screen is loaded by the gamemanager
     public void Hit(float damage) {
         health -= damage;
         healthText.text = "HEALTH: " + health.ToString();
